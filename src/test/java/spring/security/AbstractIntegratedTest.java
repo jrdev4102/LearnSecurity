c0be8bce52cf3c68732fede5920fca37a8c785a7
Changhoon-Han <hch4102@naver.com>
2021-02-24 19:39:28 +0900

    Initial commit

---
+++ src/test/java/spring/security/AbstractIntegratedTest.java
@@ -0,0 +1,8 @@
+package spring.security;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public abstract class AbstractIntegratedTest {
+
+}
---
+++ src/test/java/spring/security/AbstractMockMvcTest.java
@@ -0,0 +1,9 @@
+package spring.security;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public abstract class AbstractMockMvcTest {
+
+}
