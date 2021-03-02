751c9f822606b942a7677f24a09a48b93e9a049a
Changhoon-Han <hch4102@naver.com>
2021-03-02 14:27:13 +0900

    ...

--- src/main/java/spring/security/security/InitDatabase.java
+++ src/main/java/spring/security/security/InitDatabase.java
@@ -17,9 +17,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Application 시작 시 초기 데이터 설정을 진행하고 로그를 기록하는 클래스
- */
 @Component
 @RequiredArgsConstructor
 public class InitDatabase implements ApplicationListener<ContextRefreshedEvent> {
@@ -50,9 +47,6 @@ public class InitDatabase implements ApplicationListener<ContextRefreshedEvent> 
 
     }
 
-    /**
-     * Application 시작 시 초기 데이터 설정을 정의한 클래스
-     */
     @Service
     @RequiredArgsConstructor
     static class InitService {
